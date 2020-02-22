cbe2c31a623f344a425fbe1198d9196060955376
askubicz <61337789+askubicz@users.noreply.github.com>
2020-02-21 21:09:21 -0500

    Add files via upload

---
+++ create_events.php
@@ -0,0 +1,99 @@
+<?php
+     
+    require 'database.php';
+ 
+    if ( !empty($_POST)) {
+        // keep track validation errors
+        $nameError = null;
+        $dateError = null;
+        $locationError = null;
+         
+        // keep track post values
+        $name = $_POST['name'];
+        $date = $_POST['date'];
+        $location = $_POST['location'];
+         
+        // validate input
+        $valid = true;
+        if (empty($name)) {
+            $nameError = 'Please enter Name';
+            $valid = false;
+        }
+         
+        if (empty($date)) {
+            $dateError = 'Please enter date Address';
+            $valid = false;
+        }
+         
+        if (empty($location)) {
+            $locationError = 'Please enter Location';
+            $valid = false;
+        }
+         
+        // insert data
+        if ($valid) {
+            $pdo = Database::connect();
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "INSERT INTO events (name,date,location) values(?, ?, ?)";
+            $q = $pdo->prepare($sql);
+            $q->execute(array($name,$date,$location));
+            Database::disconnect();
+            header("Location: events.php");
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link   href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.min.js"></script>
+</head>
+ 
+<body>
+    <div class="container">
+     
+                <div class="span10 offset1">
+                    <div class="row">
+                        <h3>Create a Customer</h3>
+                    </div>
+             
+                    <form class="form-horizontal" action="create_events.php" method="post">
+                      <div class="control-group <?php echo !empty($nameError)?'error':'';?>">
+                        <label class="control-label">Name</label>
+                        <div class="controls">
+                            <input name="name" type="text"  placeholder="Name" value="<?php echo !empty($name)?$name:'';?>">
+                            <?php if (!empty($nameError)): ?>
+                                <span class="help-inline"><?php echo $nameError;?></span>
+                            <?php endif; ?>
+                        </div>
+                      </div>
+                      <div class="control-group <?php echo !empty($dateError)?'error':'';?>">
+                        <label class="control-label">Date</label>
+                        <div class="controls">
+                            <input name="date" type="text" placeholder="Date" value="<?php echo !empty($date)?$date:'';?>">
+                            <?php if (!empty($dateError)): ?>
+                                <span class="help-inline"><?php echo $dateError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+                      <div class="control-group <?php echo !empty($locationError)?'error':'';?>">
+                        <label class="control-label">Location</label>
+                        <div class="controls">
+                            <input name="location" type="text"  placeholder="Location" value="<?php echo !empty($location)?$location:'';?>">
+                            <?php if (!empty($locationError)): ?>
+                                <span class="help-inline"><?php echo $locationError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+                      <div class="form-actions">
+                          <button type="submit" class="btn btn-success">Create</button>
+                          <a class="btn" href="events.php">Back</a>
+                        </div>
+                    </form>
+                </div>
+                 
+    </div> <!-- /container -->
+  </body>
+</html>
